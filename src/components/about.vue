2e7685304ce09821d0b160ea816db7f1b2eda400
uniquker <352262437@qq.com>
2017-07-10 11:16:39 +0800

    Create about.vue

---
+++ src/components/about.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+  	<img src="/static/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your about App page!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
